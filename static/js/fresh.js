3f10919a8141c2506ba804e9cedba00112fa8991
Ed Hemphill <edhemphill@gmail.com>
2022-08-18 23:51:47 -0500

    Fix fresh loader overflow
    overflow: visible was making uwanted horiz scrolling on mobile

--- static/js/fresh.js
+++ static/js/fresh.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $(window).on('load', function() { // makes sure the whole site is loaded 
         $('#status').fadeOut(); // will first fade out the loading animation 
         $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-        $('body').delay(350).css({'overflow':'visible'});
+ //       $('body').delay(350).css({'overflow':'visible'});
     })
 
     //Mobile menu toggle
